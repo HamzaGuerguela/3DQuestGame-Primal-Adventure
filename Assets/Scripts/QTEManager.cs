5e4b5dca48053f21860dab6f713caafb58f76c72
HamzaGuerguela <100441492+HamzaGuerguela@users.noreply.github.com>
2022-12-02 00:34:41 +0100

    Added Timeline Robot Model

---
+++ Assets/Scripts/QTEManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QTEManager : MonoBehaviour
+{
+    #region Inspector
+
+    
+
+    #endregion
+
+    #region Unity Event Functions
+
+    
+
+    #endregion
+}
